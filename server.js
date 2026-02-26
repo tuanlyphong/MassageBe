3509f2e7bc02cbfcaf99ab6eaa78d604d9d1841f
tuanlyphong <tuanphong366@gmail.com>
2026-02-27 01:11:56 +0700

    fix3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
-const { query, poor } = require("./config/database");
+const { query, pool } = require("./config/database");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
