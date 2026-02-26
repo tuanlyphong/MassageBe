0ab3d164c61bae800f0ad1570eac3ea255472f9a
tuanlyphong <tuanphong366@gmail.com>
2026-02-27 00:25:38 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,17 +1,9 @@
 const { Pool } = require("pg");
-
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
 });
 pool.on("connect", () => {
   console.log("✅ Connected to PostgreSQL database");
