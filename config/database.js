87c119d8f2fc68837c643a629ce9a9f291ca448b
tuanlyphong <tuanphong366@gmail.com>
2026-02-27 00:14:08 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,16 +1,18 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT || 5432,
-  database: process.env.DB_NAME || "massage_device_db",
-  user: process.env.DB_USER || "postgres",
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
 });
-
 pool.on("connect", () => {
   console.log("✅ Connected to PostgreSQL database");
 });
